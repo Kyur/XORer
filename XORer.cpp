a7989f4286ce0780f2c6408157e99e0810836c8e
Kur <kkw90111@naver.com>
2018-09-16 23:19:32 +0900

    second

--- XORer.cpp
+++ XORer.cpp
@@ -25,8 +25,8 @@ int _tmain(int argc, TCHAR** argv)
 	int ch = '\\';
 
 
-
-	if(argc != 3 && argc != 4)
+	// Check options
+	if (argc != 3 && argc != 4)
 	{
 		printf(" \n [!] Usage: XORer.exe [Option] [Target File Path] [XOR key]\n");
 		printf(" \n\t-d,\tCalculate XOR by 4 byte.\n");
@@ -37,28 +37,32 @@ int _tmain(int argc, TCHAR** argv)
 		return -1;
 	}
 
-	if(argc == 4)
+
+	if (argc == 4)
 	{
-		// selected option
-		
-		if(strncmp(argv[1], "-d", 0x02) != 0 && strncmp(argv[1], "-w", 0x02) != 0 && strncmp(argv[1], "-b", 0x02) != 0)
+		// when selected option, set key size
+
+		if (strncmp(argv[1], "-d", 0x02) != 0 && strncmp(argv[1], "-w", 0x02) != 0 && strncmp(argv[1], "-b", 0x02) != 0)
 		{
 			printf(" [!] ERROR: Worng Arguments\n");
 			return -1;
 		}
 
 		// Read XOR_KEY (argv[3])
-		xorKey = strtol((char *)argv[3], (char **)&endPtr, HEX_BASE);
+		xorKey = (DWORD)strtoll((char *)argv[3], (char **)&endPtr, HEX_BASE);
+
 		strncpy(arrTargetFileName, argv[2], strlen(argv[2]));
 		index = (TCHAR*)strstr(argv[1], "-") + 1;
 		bOption = TRUE;
 	}
 	else
 	{
+		// when not selected option, set default 4byte key size
 		// Read XOR_KEY (argv[2])
-		xorKey = strtol((char *)argv[2], (char **)&endPtr, HEX_BASE);
+		xorKey = strtoll((char *)argv[2], (char **)&endPtr, HEX_BASE);
 		strncpy(arrTargetFileName, argv[1], strlen(argv[1]));
 	}
+
 
 	GetModuleFileName(NULL, arrCurPath, MAX_PATH);
 	pFileName = strrchr(arrCurPath, ch);
@@ -66,19 +70,20 @@ int _tmain(int argc, TCHAR** argv)
 	memcpy(arrNewFile, arrCurPath, MAX_PATH);
 
 	hOriFile = CreateFile(arrTargetFileName, GENERIC_READ, NULL, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	hNewFile = CreateFile(arrNewFile, GENERIC_READ|GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	hNewFile = CreateFile(arrNewFile, GENERIC_READ | GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-	if( (hOriFile == INVALID_HANDLE_VALUE)  || (hNewFile == INVALID_HANDLE_VALUE) )
+	if ((hOriFile == INVALID_HANDLE_VALUE) || (hNewFile == INVALID_HANDLE_VALUE))
 	{
 		printf(" [!] CreateFile- ERROR %08X\n", GetLastError());
 		return -1;
 	}
 
+	// Allocate memory for read file
 	fileSize = GetFileSize(hOriFile, NULL);
-	lpBuffer = (PBYTE)VirtualAlloc(NULL, fileSize, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
-	lpNewBuffer = (PBYTE)VirtualAlloc(NULL, fileSize, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
+	lpBuffer = (PBYTE)VirtualAlloc(NULL, fileSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	lpNewBuffer = (PBYTE)VirtualAlloc(NULL, fileSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-	if(lpBuffer == NULL || lpNewBuffer == NULL)
+	if (lpBuffer == NULL || lpNewBuffer == NULL)
 	{
 		printf(" [!] VirtualAlloc- ERROR %08X\n", GetLastError());
 		return -1;
@@ -86,66 +91,67 @@ int _tmain(int argc, TCHAR** argv)
 
 	// Read Original file
 	ReadFile(hOriFile, lpBuffer, fileSize, &lpNumberOfBytesRead, NULL);
-	if(lpNumberOfBytesRead == 0)
+	if (lpNumberOfBytesRead == 0)
 	{
 		printf(" [!] ReadFile- ERROR %08X\n", GetLastError());
 		return -1;
 	}
-	
-	if(bOption)
+
+
+	// Calculate XOR
+	if (bOption)
 	{
-		switch(*index)
+		switch (*index)
 		{
 		case 'd':
-			for(cnt = 0; cnt < fileSize; cnt + 4)
+			for (cnt = 0; cnt < fileSize; cnt += 4)
 			{
 				*(PDWORD)(lpNewBuffer + cnt) = *(PDWORD)(lpBuffer + cnt) ^ xorKey;
-				cnt += 4;
 			}
-		break;
+			break;
 
 		case 'w':
-			for(cnt = 0; cnt < fileSize; cnt + 2)
+			for (cnt = 0; cnt < fileSize; cnt += 2)
 			{
 				*(PWORD)(lpNewBuffer + cnt) = *(PWORD)(lpBuffer + cnt) ^ (WORD)xorKey;
-				cnt += 2;
 			}
-		break;
+			break;
 		case 'b':
-			for(cnt = 0; cnt < fileSize; cnt + 1)
+			for (cnt = 0; cnt < fileSize; cnt += 1)
 			{
 				*(PBYTE)(lpNewBuffer + cnt) = *(PBYTE)(lpBuffer + cnt) ^ (BYTE)xorKey;
-				cnt += 1;
 			}
-		break;
+			break;
 		default:
 			printf(" [!] ERROR: argument\n");
-		break;
+			break;
 		}
 	}
 	else
 	{
-		for(cnt = 0; cnt < fileSize; cnt + 4)
+		for (cnt = 0; cnt < fileSize; cnt += 4)
 		{
 			*(PDWORD)(lpNewBuffer + cnt) = *(PDWORD)(lpBuffer + cnt) ^ xorKey;
-			cnt += 4;
 		}
 	}
-	
-	// Write XORed data file
+
+	printf(" [*] XOR Completed. \n");
+
+
+	// Write XORed data to new file
 	WriteFile(hNewFile, lpNewBuffer, fileSize, &lpNumberOfBytesRead, NULL);
-	if(lpNumberOfBytesRead <= 0)
+	if (lpNumberOfBytesRead <= 0)
 	{
 		printf(" [!] ERROR: %08X, %08X", GetLastError(), lpNumberOfBytesRead);
 	}
+
+	printf(" [*] Write xor data to [%s]. \n", arrNewFile);
+
 
 	VirtualFree(lpBuffer, fileSize, MEM_RELEASE);
 	VirtualFree(lpNewBuffer, fileSize, MEM_RELEASE);
 	CloseHandle(hOriFile);
 	CloseHandle(hNewFile);
-
-	printf(" [*] XOR Completed %s. \n", arrNewFile);
-	printf(" [*] Plz press any key. \n");
 
 	return 0;
 }
